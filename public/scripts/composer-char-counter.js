e33ae2a8feaa5629d5a3224bc3725ec7fb6836ae
Mehwish Iqbal <mehwishned21@gmail.com>
2023-08-20 16:51:26 -0500

    Fix errors and formatting

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,19 +3,11 @@ $(document).ready(function () {
     //get length of textarea
     let charlen = $(this).val().length;
 
-    //if length is equal to 0 then show error text
-    if (charlen === 0) {
-      $("#error-message").show();
-      $("#error-message").text("No content found! Please enter some text.");
-    }
     //if length is greater than 140 then show error text
     if (charlen > 140) {
       $(".counter").css("color", "red");
-      $("#error-message").show();
-      $("#error-message").text(
-        "The number of characters has exceeded the limit!"
-      );
     } else {
+      //show the counter in default color black
       $(".counter").css("color", "black");
     }
 
