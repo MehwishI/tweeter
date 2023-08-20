607ab26cfeba47008637e3bc28f78d80164fb436
Mehwish Iqbal <mehwishned21@gmail.com>
2023-08-19 21:42:34 -0500

    Add comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
   $("textarea").on("input", function () {
+    //get length of textarea
     let charlen = $(this).val().length;
 
+    //if length is equal to 0 then show error text
     if (charlen === 0) {
       $("#error-message").show();
       $("#error-message").text("No content found! Please enter some text.");
     }
+    //if length is greater than 140 then show error text
     if (charlen > 140) {
       $(".counter").css("color", "red");
       $("#error-message").show();
@@ -16,6 +19,7 @@ $(document).ready(function () {
       $(".counter").css("color", "black");
     }
 
+    //display counter with characters left with a negative sign
     $(".counter").val(140 - charlen);
   });
 });
